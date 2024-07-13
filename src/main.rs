69399c022a33eb9589940963b93b2bdb16572522
单子亨 <brianshan974@gmail.com>
2024-07-13 16:20:29 +0800

    Now returns either 200 or 404.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
 use std::{
     error::Error,
-    io::Write,
+    io::{Read, Write},
     net::{TcpListener, TcpStream},
 };
 
 fn handle_connection(stream: &mut TcpStream) -> Result<(), Box<dyn Error>> {
-    let response = "HTTP/1.1 200 OK\r\n\r\n".as_bytes();
+    let successful_response = "HTTP/1.1 200 OK\r\n\r\n".as_bytes();
+    let failure_response = "HTTP/1.1 404 Not Found\r\n\r\n".as_bytes();
+
+    let mut request = String::new();
+    stream.read_to_string(&mut request)?;
+
+    let mut request_line = request.split("\r\n").next().unwrap().split(' ');
+    let _request_type = request_line.next();
+    let request_path = request_line.next().unwrap();
+
+    let response = if request_path != "/" {
+        failure_response
+    } else {
+        successful_response
+    };
+
     stream.write_all(response)?;
     Ok(())
 }
